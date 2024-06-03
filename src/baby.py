8d07423300d6dc8b00958ba56082e188c9eb3a56
h-0-0 <hwbourne@gmail.com>
2024-06-03 21:52:39 +0100

    Got something running

--- src/baby.py
+++ src/baby.py
@@ -3,8 +3,6 @@ from train import train
 from naive_data import naive_get_data
 
 
-
-
 def run():
     # Get data
     data = naive_get_data()
--- src/train.py
+++ src/train.py
@@ -1,19 +1,27 @@
 from datasets import Dataset
 import jax
-
+import numpy as np
+import xgboost as xgb
 
 # Train model
-def train(model, data):
-    data = data.to_numpy()
-    ds = Dataset.from_dict({"data": data})
+def train(model, X, y, num_epochs=10, batch_size=512):
+    X = X.to_numpy()
+    y = y.to_numpy()
+    ds = Dataset.from_dict({"X": X, "y": y})
     ds = ds.with_format("jax")  
-    dl = ds.dataloader(batch_size=32, shuffle=True)
     # Begin Training!
     for epoch in range(num_epochs):
-        subkey = np.asarray(jax.random.fold_in(key, epoch))
+        print(f"Epoch {epoch}")
         ds.shuffle(seed=epoch)
-        epoch_loss = []
         for batch_num, batch in enumerate(ds.iter(batch_size=batch_size)):
-            batch_x = batch['data']
-            X_train, y_train = 
+            print(f"Batch {batch_num}")
+            X_train, y_train = batch['X'], batch['y']
             model.fit(X_train, y_train)
+
+# If script is run directly, we will run a training trial
+if __name__ == '__main__':
+    from xgb import get_xgb
+    from naive_data import naive_get_data
+    X, y = naive_get_data()
+    model = get_xgb()
+    train(model, X, y)
