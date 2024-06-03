1b74767d706c848d85cd0b982270f78e4538d9ca
sambowyer <sambowyer2@gmail.com>
2024-06-03 17:36:19 +0100

    Removed faulty label-removal'

--- src/preprocess.py
+++ src/preprocess.py
@@ -65,11 +65,11 @@ def remove_labels(df, human_labelled_proportion, human_labelled_positive_proport
     true_negatives = df['fraud_bool'] == 0
 
     # remove labels from true positives
-    true_positives_to_remove = true_positives.sample(len(true_positives) - int(human_labelled_positive_proportion*human_labelled_proportion * len(true_positives)))
+    true_positives_to_remove = df[true_positives].sample(len(true_positives) - int(human_labelled_positive_proportion*human_labelled_proportion * len(true_positives)))
     true_negatives_to_remove = true_negatives.sample(len(true_negatives) - int((1-human_labelled_positive_proportion)*human_labelled_proportion * len(true_negatives)))
     
     # remove fraud_bool for entries indexed by true_positives_to_remove or true_negatives_to_kepp (set to np.nan)
-    df.loc[true_positives_to_remove, 'fruad_bool'] = np.nan 
+    df.loc[true_positives_to_remove] = np.nan 
     df.loc[true_negatives_to_remove, 'fraud_bool'] = np.nan
 
     return df
@@ -86,11 +86,12 @@ if __name__ == '__main__':
     human_labelled_proportion = 0.01,
     human_labelled_positive_proportion = 0.4
 
-    df = remove_labels(df,
-                       human_labelled_proportion=human_labelled_positive_proportion,
-                       human_labelled_positive_proportion=human_labelled_positive_proportion) 
+    # put back in when working correctly
+    # df = remove_labels(df,
+    #                    human_labelled_proportion=human_labelled_positive_proportion,
+    #                    human_labelled_positive_proportion=human_labelled_positive_proportion) 
     
-    breakpoint()
+    # breakpoint()
 
     # Split the data
     train_test_split(df)
