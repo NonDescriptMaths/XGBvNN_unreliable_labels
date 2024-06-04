debe438283016f3df5eff0cfed430d15f038c531
h-0-0 <hwbourne@gmail.com>
2024-06-04 13:23:06 +0100

    General bits ready, now need neural net, metrics and active learning dataset

--- src/train.py
+++ src/train.py
@@ -3,34 +3,54 @@ import jax
 import numpy as np
 import xgboost as xgb
 
-# Train model
-def train(model, X, y, num_epochs=10, batch_size=512):
-    X = X.to_numpy()
-    y = y.to_numpy()
+def pretrain(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarray):
     ds = Dataset.from_dict({"X": X, "y": y})
     ds = ds.with_format("jax")  
 
-    # do initial training
-    # model.fit(X[:10], y[:10])  # Warmup
+    # Begin Training!
+    ds.shuffle(seed=0)
+    X_train, y_train = ds['X'], ds['y']
+    model.fit(X_train, y_train, eval_metric="logloss", eval_set=[(X_test, y_test)])
+    metrics = model.evals_result()
+    return metrics
+
+# Train model
+def train(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarray, num_epochs=10, batch_size='max'):
+    ds = Dataset.from_dict({"X": X, "y": y})
+    ds = ds.with_format("jax")  
 
     # Begin Training!
+    if batch_size == 'max':
+        batch_size = len(X)
+    all_metrics = []
     for epoch in range(num_epochs):
         print(f"Epoch {epoch}")
         ds.shuffle(seed=epoch)
         for batch_num, batch in enumerate(ds.iter(batch_size=batch_size)):
             print(f"Batch {batch_num}")
             X_train, y_train = batch['X'], batch['y']
-            model.fit(X_train, y_train)
-
-            #if iteration % 10 == 0:
-            #     model.fit()
-            # else: 
-            #     model.update()
+            if (epoch % 10 == 0) and (batch_num == 0):
+                pretrain(model, X_train, y_train, X_test, y_test)
+                metric = model.evals_result()
+            else: 
+                model.update(X_train, y_train, eval_metric="logloss", eval_set=[(X_test, y_test)])
+                metric = model.evals_result()
+            all_metrics.append(metric)
+    return all_metrics
 
 # If script is run directly, we will run a training trial
 if __name__ == '__main__':
     from xgb import get_xgb
     from naive_data import naive_get_data
-    X, y = naive_get_data()
+    X, y, X_test, y_test = naive_get_data()
+    X = X.to_numpy()
+    y = y.to_numpy()
+    X_test = X_test.to_numpy()
+    y_test = y_test.to_numpy()
     model = get_xgb()
-    train(model, X, y)
+    # Test pretraining
+    results = pretrain(model, X, y, X_test, y_test)
+    print(results)
+    # Test training
+    results  =train(model, X, y, X_test, y_test, num_epochs=3)
+    print(results)
--- src/xgb.py
+++ src/xgb.py
@@ -6,6 +6,23 @@ import jax.numpy as jnp
 from typing import Callable
 from optax.losses import sigmoid_binary_cross_entropy
 
+# Wrapper for the xgb model that implements an update method
+class XGBWrapper:
+    def __init__(self, model):
+        self.model = model
+
+    def update(self, X, y, eval_metric="logloss", eval_set=None):
+        return self.model.fit(X, y, eval_metric=eval_metric, eval_set=eval_set, xgb_model=self.model)
+
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    def fit(self, X, y, eval_metric="logloss", eval_set=None):
+        return self.model.fit(X, y, eval_metric=eval_metric, eval_set=eval_set)
+    
+    def evals_result(self):
+        return self.model.evals_result()
+
 
 def hvp(f, inputs, vectors):
     """Hessian-vector product."""
@@ -29,4 +46,4 @@ def jax_autodiff_grad_hess(
 # Create xgb model
 def get_xgb(lr=0.1, loss_function: Callable[[np.ndarray, np.ndarray], np.ndarray] = sigmoid_binary_cross_entropy):
     jax_objective = jax.jit(partial(jax_autodiff_grad_hess, loss_function))
-    return XGBClassifier(objective=jax_objective, n_estimators=100, max_depth=2, learning_rate=lr)
+    return XGBWrapper(XGBClassifier(objective=jax_objective, n_estimators=100, max_depth=2, learning_rate=lr))
