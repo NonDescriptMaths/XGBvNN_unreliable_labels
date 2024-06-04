c774ecf1fc12a913d885f95859819337b68f6313
Kieran Morris <kieranmorris77@gmail.com>
2024-06-04 16:21:21 +0100

    add shannon entropy

--- src/query.py
+++ src/query.py
@@ -1,3 +1,6 @@
+import jax.numpy as jnp
+
+
 def cross_entropy(model, K, unlabelled_data, labelled_data=None):
     '''
     model: prediction model
@@ -7,12 +10,36 @@ def cross_entropy(model, K, unlabelled_data, labelled_data=None):
     '''
     predictions = model.predict(unlabelled_data)
 
-    ce = predictions.cross_extropy()
+    ce = predictions.cross_entropy()
 
     #pick top K samples
     ce.sort()
 
     return ce[:K]
+
+def shannon_entropy(model, K, unlabelled_data, labelled_data=None):
+    '''
+    model: prediction model
+    K: number of queries to select
+    unlabelled_data: currently unlabelled data
+    labelled_data: currently labelled data (may not be used)
+    '''
+    predictions = model.predict(unlabelled_data)
+    '''
+    predictions: (x,y,y_hat) for 0 <= y_hat <=1
+    '''
+    # Calculate the entropy of the predictions
+    p = predictions[2]
+    p = jnp.clip(p, 1e-10, 1 - 1e-10)
+    entropy = -p * jnp.log2(p) - (1 - p) * jnp.log2(1 - p)
+
+    # Sort the entropy values in descending order and get the indices
+    indices = jnp.argsort(-entropy)
+
+    # Return the top K samples
+    return unlabelled_data[indices[:K]]
+
+
 
 
 def random(model, K, unlabelled_data, labelled_data=None):
---
+++ src/query_testing.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from query import shannon_entropy
+import numpy as np
+
+# Generate a binary classification dataset with 2-dimensional covariates
+X, y = make_classification(n_samples=200, n_features=2, n_informative=2, n_redundant=0, random_state=1)
+
+# Split the data into labelled and unlabelled
+X_labelled = X[:100]
+y_labelled = y[:100]
+X_unlabelled = X[100:]
+
+# Train a logistic regression model on the labelled data
+model = LogisticRegression()
+model.fit(X_labelled, y_labelled)
+
+# Define a predict method for the model that returns (x, y, y_hat)
+def predict(data):
+    y_hat = model.predict_proba(data)[:, 1]
+    return (data, np.zeros_like(y_hat), y_hat)
+
+# Replace the model's predict method with our custom one
+model.predict = predict
+
+# Use the shannon_entropy function with this model and the unlabelled data
+selected_samples = shannon_entropy(model, 50, X)
+
+# Plot the decision boundary
+plt.figure(figsize=(10, 6))
+plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=plt.cm.Paired)
+ax = plt.gca()
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+xx = np.linspace(xlim[0], xlim[1], 30)
+yy = np.linspace(ylim[0], ylim[1], 30)
+YY, XX = np.meshgrid(yy, xx)
+xy = np.vstack([XX.ravel(), YY.ravel()]).T
+Z = model.decision_function(xy).reshape(XX.shape)
+ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+
+# Plot the points with maximum entropy in dark green
+plt.scatter(selected_samples[:, 0], selected_samples[:, 1], color='darkgreen')
+
+plt.show()
