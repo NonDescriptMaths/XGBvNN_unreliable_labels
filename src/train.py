953fd43e00db4ee46c4138c9e5ec53ae645f7066
h-0-0 <hwbourne@gmail.com>
2024-06-06 17:31:40 +0100

    Modified __main__ so now uses get_data as opposed to naive_get_data

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from datasets import Dataset
 import jax
 import numpy as np
 import xgboost as xgb
+from utils import get_data, get_X_y, get_X_y_labelled
 
 def pretrain(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarray):
     ds = Dataset.from_dict({"X": X, "y": y})
@@ -15,7 +16,7 @@ def pretrain(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.nda
     return metrics
 
 # Train model
-def train(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarray, num_epochs=10, batch_size='max'):
+def train(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarray, num_epochs=20, batch_size='max'):
     ds = Dataset.from_dict({"X": X, "y": y})
     ds = ds.with_format("jax")  
 
@@ -45,15 +46,19 @@ def train(model, X:np.ndarray, y:np.ndarray, X_test:np.ndarray, y_test:np.ndarra
 # If script is run directly, we will run a training trial
 if __name__ == '__main__':
     from xgb import get_xgb
-    from naive_data import naive_get_data
-    X, y, X_test, y_test = naive_get_data()
-    X = X.to_numpy()
-    y = y.to_numpy()
+    train_data, test_data, validate_data = get_data()
+    X_train, y_train = get_X_y_labelled(train_data)
+    X_val, y_val = get_X_y_labelled(validate_data)
+    X_test, y_test = get_X_y_labelled(test_data)
+
+    X_train = X_train.to_numpy()
+    y_train = y_train.to_numpy()
     X_test = X_test.to_numpy()
     y_test = y_test.to_numpy()
+
     model = get_xgb()
     # Test pretraining
-    results = pretrain(model, X, y, X_test, y_test)
+    results = pretrain(model, X_train, y_train, X_test, y_test)
     print(results)
     # Create plots directory if it doesn't exist
     import os
@@ -80,5 +85,5 @@ if __name__ == '__main__':
     plt.close()
 
     # Test training
-    results = train(model, X, y, X_test, y_test, num_epochs=3)
-    print(results)
+    results = train(model, X_train, y_train, X_test, y_test, num_epochs=20)
+    
